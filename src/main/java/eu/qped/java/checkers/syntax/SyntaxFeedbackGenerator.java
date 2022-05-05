dd147341917355794c770212c77e479aa13606a1
Ajio <Ajio@students.uni-marburg.de>
2022-05-05 16:03:38 +0200

    test Feedback

--- src/main/java/eu/qped/java/checkers/syntax/SyntaxFeedbackGenerator.java
+++ src/main/java/eu/qped/java/checkers/syntax/SyntaxFeedbackGenerator.java
@@ -307,7 +307,7 @@ public class SyntaxFeedbackGenerator {
                         result.append("You used the braces incorrectly when declaring a method.");
                     } else {
                         result
-                                .append("Every java statement must end with a Semicolon <html><p color=\"red\"> ; </p></html> . ")
+                                .append("Every java statement must end with a Semicolon. ")
                                 .append(NEW_LINE);
                                 setExample(" for example :int oddNumber = 7;");
                     }
