e8e57d6cf21b1f3f2b7e2109062e8a6be693bb43
Ajio <Ajio@students.uni-marburg.de>
2022-05-05 15:54:38 +0200

    test Feedback

--- src/main/java/eu/qped/java/checkers/syntax/SyntaxFeedbackGenerator.java
+++ src/main/java/eu/qped/java/checkers/syntax/SyntaxFeedbackGenerator.java
@@ -307,7 +307,7 @@ public class SyntaxFeedbackGenerator {
                         result.append("You used the braces incorrectly when declaring a method.");
                     } else {
                         result
-                                .append("Every java statement must end with a Semicolon. ")
+                                .append("Every java statement must end with a Semicolon <p color=\"red\"> ; </p>. ")
                                 .append(NEW_LINE);
                                 setExample(" for example :int oddNumber = 7;");
                     }
