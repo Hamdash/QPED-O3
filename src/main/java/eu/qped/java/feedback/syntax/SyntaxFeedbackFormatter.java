965dfaf668f9cead6425f81cd56452f6b766b56b
Ajio <Ajio@students.uni-marburg.de>
2022-05-21 16:21:22 +0200

    add test Classes

--- src/main/java/eu/qped/java/feedback/syntax/SyntaxFeedbackFormatter.java
+++ src/main/java/eu/qped/java/feedback/syntax/SyntaxFeedbackFormatter.java
@@ -9,17 +9,19 @@ import java.util.stream.Collectors;
 public final class SyntaxFeedbackFormatter {
 
     public static SyntaxFeedbackNew formatFeedback(SyntaxFeedbackNew syntaxFeedback) {
-        syntaxFeedback.setHeader(formatHeader(syntaxFeedback.getHeader()));
-        syntaxFeedback.setFeedbackMessage(formatFeedbackMessage(syntaxFeedback.getFeedbackMessage()));
-        syntaxFeedback.setErrorLine(formatErrorLine(syntaxFeedback.getErrorLine()));
-        syntaxFeedback.setErrorSource(formatErrorSource(syntaxFeedback.getErrorSource()));
-        syntaxFeedback.setSolutionExample(buildFeedbackSolutionExample(syntaxFeedback.getSolutionExample()));
+        if(syntaxFeedback != null) {
+            syntaxFeedback.setHeader(formatHeader(syntaxFeedback.getHeader()));
+            syntaxFeedback.setFeedbackMessage(formatFeedbackMessage(syntaxFeedback.getFeedbackMessage()));
+            syntaxFeedback.setErrorLine(formatErrorLine(syntaxFeedback.getErrorLine()));
+            syntaxFeedback.setErrorSource(formatErrorSource(syntaxFeedback.getErrorSource()));
+            syntaxFeedback.setSolutionExample(buildFeedbackSolutionExample(syntaxFeedback.getSolutionExample()));
+        }
         return syntaxFeedback;
     }
 
 
     private static String formatHeader(String feedbackHeader) {
-        return (!feedbackHeader.equals("") ? ""
+        return (feedbackHeader != null && !feedbackHeader.equals("") ? ""
                 + MarkdownFormatterUtility.asHeading2(feedbackHeader)
                 : ""
         );
@@ -27,7 +29,7 @@ public final class SyntaxFeedbackFormatter {
 
 
     private static String formatFeedbackMessage(String feedbackMessage) {
-        return (!feedbackMessage.equals("") ? ""
+        return (feedbackMessage != null && !feedbackMessage.equals("") ? ""
                 + Arrays.stream(feedbackMessage.split(MarkdownFormatterUtility.NEW_Double_LINE))
                 .map(line -> MarkdownFormatterUtility.asBold(line.trim()))
                 .collect(Collectors.joining(""))
@@ -36,7 +38,7 @@ public final class SyntaxFeedbackFormatter {
     }
 
     private static String formatErrorLine(String errorLine) {
-        return (!errorLine.equals("") ? ""
+        return (errorLine != null && !errorLine.equals("") ? ""
                 + MarkdownFormatterUtility.asBold(errorLine)
                 : ""
         );
@@ -44,7 +46,7 @@ public final class SyntaxFeedbackFormatter {
 
 
     private static String formatErrorSource(String errorSource) {
-        return (!errorSource.equals("") ? ""
+        return (errorSource != null && !errorSource.equals("") ? ""
                 + MarkdownFormatterUtility.asBold("Where this error happens:")
                 + MarkdownFormatterUtility.asJavaCodeBlock(errorSource)
                 : ""
@@ -53,7 +55,7 @@ public final class SyntaxFeedbackFormatter {
 
 
     private static String buildFeedbackSolutionExample(String solutionExample) {
-        return (!solutionExample.equals("") ? ""
+        return (solutionExample != null && !solutionExample.equals("") ? ""
                 + MarkdownFormatterUtility.asBold("Example to fix this error:")
                 + MarkdownFormatterUtility.asJavaCodeBlock(solutionExample)
                 : ""
--- src/main/java/eu/qped/java/feedback/syntax/SyntaxFeedbackGeneratorNew.java
+++ src/main/java/eu/qped/java/feedback/syntax/SyntaxFeedbackGeneratorNew.java
@@ -20,12 +20,12 @@ public class SyntaxFeedbackGeneratorNew extends AbstractFeedbackGenerator {
     private final AtomicInteger feedbackCounter = new AtomicInteger(0);
 
     @Override
-    public String generateHeader() {
+    protected String generateHeader() {
         return "Error " + String.format("%02d", feedbackCounter.incrementAndGet()) + ":";
     }
 
     @Override
-    public String generateFeedbackMessage(SyntaxError error) {
+    protected String generateFeedbackMessage(SyntaxError error) {
         String result = "";
         SyntaxFeedbackMessages syntaxFeedbackMessages = SyntaxFeedbackMessages.builder().build();
         if (syntaxFeedbackMessages.getFeedbackMessagesByErrorCode().containsKey(error.getErrorCode())) {
@@ -41,17 +41,17 @@ public class SyntaxFeedbackGeneratorNew extends AbstractFeedbackGenerator {
     }
 
     @Override
-    public String generateErrorLine(SyntaxError syntaxError) {
+    protected String generateErrorLine(SyntaxError syntaxError) {
         return "At line :" + String.format("%d", syntaxError.getLine());
     }
 
     @Override
-    public String generateErrorSource(SyntaxError syntaxError) {
+    protected String generateErrorSource(SyntaxError syntaxError) {
         return syntaxError.getErrorSourceCode();
     }
 
     @Override
-    public String generateSolutionExample(SyntaxError error) {
+    protected String generateSolutionExample(SyntaxError error) {
         String result = "";
         SyntaxFeedbackSolutionExamples syntaxFeedbackSolutionExamples = SyntaxFeedbackSolutionExamples.builder().build();
         if (syntaxFeedbackSolutionExamples.getSolutionExamplesByErrorCode().containsKey(error.getErrorCode())) {
@@ -70,46 +70,46 @@ public class SyntaxFeedbackGeneratorNew extends AbstractFeedbackGenerator {
 
 
 
-
-    public static void main(String[] args) {
-        String code = ""
-                + "public static void main (String[] args) { \n"
-                + " int i = 0; b = 0;  \n"
-                + "  \n"
-                + "} \n";
-
-
-        SyntaxChecker syntaxChecker = SyntaxChecker.builder().stringAnswer(code).level(CheckLevel.ADVANCED).build();
-        Map<String, String> mainSettings = new HashMap<>();
-        mainSettings.put("semanticNeeded", "false");
-        mainSettings.put("syntaxLevel", "2");
-        mainSettings.put("preferredLanguage", "en");
-        mainSettings.put("styleNeeded", "false");
-
-        MainSettings mainSettingsConfiguratorConf = new MainSettings(mainSettings);
-
-        MassExecutor massE = new MassExecutor(null, null, syntaxChecker, mainSettingsConfiguratorConf);
-        massE.execute();
-
-        List<String> result = new ArrayList<>();
-
-//        for (SyntaxFeedback syntax : massE.getSyntaxFeedbacks()) {
+//
+//    public static void main(String[] args) {
+//        String code = ""
+//                + "public static void main (String[] args) { \n"
+//                + " int i = 0; b = 0;  \n"
+//                + "  \n"
+//                + "} \n";
+//
+//
+//        SyntaxChecker syntaxChecker = SyntaxChecker.builder().stringAnswer(code).level(CheckLevel.ADVANCED).build();
+//        Map<String, String> mainSettings = new HashMap<>();
+//        mainSettings.put("semanticNeeded", "false");
+//        mainSettings.put("syntaxLevel", "2");
+//        mainSettings.put("preferredLanguage", "en");
+//        mainSettings.put("styleNeeded", "false");
+//
+//        MainSettings mainSettingsConfiguratorConf = new MainSettings(mainSettings);
+//
+//        MassExecutor massE = new MassExecutor(null, null, syntaxChecker, mainSettingsConfiguratorConf);
+//        massE.execute();
+//
+//        List<String> result = new ArrayList<>();
+//
+////        for (SyntaxFeedback syntax : massE.getSyntaxFeedbacks()) {
+////            String s = ""
+////                    + syntax.getFeedbackContent()
+////                    + "\n\n"
+////                    + "--------------------------------------------------";
+////            System.out.println(s);
+////
+////        }
+//        for (SyntaxFeedbackNew syntax : massE.getSyntaxFeedbackNews()) {
 //            String s = ""
-//                    + syntax.getFeedbackContent()
+//                    + syntax.toString()
 //                    + "\n\n"
 //                    + "--------------------------------------------------";
 //            System.out.println(s);
 //
 //        }
-        for (SyntaxFeedbackNew syntax : massE.getSyntaxFeedbackNews()) {
-            String s = ""
-                    + syntax.toString()
-                    + "\n\n"
-                    + "--------------------------------------------------";
-            System.out.println(s);
-
-        }
-    }
+//    }
 
 
 }
