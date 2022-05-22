03347eb38e7aa639a0172779f4dad5802bf5a454
Basel <basel.alaktaa.94@gmail.com>
2022-05-22 14:09:47 +0200

    Mass

--- src/main/java/eu/qped/java/checkers/mass/Mass.java
+++ src/main/java/eu/qped/java/checkers/mass/Mass.java
@@ -104,7 +104,7 @@ public class Mass implements Checker {
         }
 
         for (SemanticFeedback semanticFeedback : semanticFeedbacks) {
-            result[i] = "semantic Feedback: " +  styleSettings.getCompLevel();
+            result[i] = "semantic Feedback: " +  styleSettings;
             result[i + 1] = semanticFeedback.getBody() + NEW_LINE
                     + "--------------------------------------------------";
             i = i + 2;
