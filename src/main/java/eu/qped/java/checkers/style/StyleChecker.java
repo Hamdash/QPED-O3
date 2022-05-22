ba59196049d7086ee7a7859cc1240d4e83816564
Basel <basel.alaktaa.94@gmail.com>
2022-05-22 14:21:04 +0200

    Mass

--- src/main/java/eu/qped/java/checkers/style/StyleChecker.java
+++ src/main/java/eu/qped/java/checkers/style/StyleChecker.java
@@ -133,7 +133,7 @@ public class StyleChecker {
 
     private void executePMD() {
         final PMDConfiguration configuration = new PMDConfiguration();
-        configuration.setInputPaths("GrayCode.java");
+        configuration.setInputPaths("meroo.java");
         configuration.setRuleSets(xmlFileManager.getFilename());
         configuration.setReportFormat("json");
         configuration.setReportFile("report.json");
